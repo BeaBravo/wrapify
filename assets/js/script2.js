eba6b8d473a292ea5b379dc04604b589919201c9
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2023-12-08 10:37:13 -0500

    Structured query requests

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -5,45 +5,43 @@ var userReviews = data.product.top_reviews;
 var reviewsData = [];
 
 
-  
-    fetch(queryURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-       
-        for (var i = 0; i < data.product.top_reviews.length; i++) {
-            var review = {
-                body: data.top_reviews[i].body,
-                rating: data.top_reviews[i].rating,
-                isGlobal: data.top_reviews[i].is_global_review
-            };
-            
-            reviewsData.push(review);
-        }
-        })
-    
-    for (var i = 0; i < reviewsData.length; i++) {
-    fconst url = 'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/';
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-            'X-RapidAPI-Host': 'twinword-sentiment-analysis.p.rapidapi.com'
-        },
-        body: new URLSearchParams({
-            text: reviewsData[i].body
-        })
+//AMAZON API  
+fetch(queryURL)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  for (var i = 0; i < data.product.top_reviews.length; i++) {
+    var review = {
+      body: data.product.top_reviews[i].body,
+      rating: data.product.top_reviews[i].rating,
+      isGlobal: data.product.top_reviews[i].is_global_review
     };
+    reviewsData.push(review);
+  }
+});
+
     
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-
-
-        
-    }
+    //SENTIMENT API
+    for (var i = 0; i < reviewsData.length; i++) {
+        const url = 'https://twinword-sentiment-analysis.p.rapidapi.com/analyze/';
+        const options = {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': 'YOUR-RAPIDAPI-KEY', // Replace with your actual RapidAPI key
+            'X-RapidAPI-Host': 'twinword-sentiment-analysis.p.rapidapi.com'
+          },
+          body: new URLSearchParams({
+            text: reviewsData[i].body
+          })
+        };
+    
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
