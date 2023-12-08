ac2b6d6ac6fc95d9cf62fab47b583bf69a2e522d
TurtleNav <tourangeau_luc@yahoo.com>
2023-12-07 19:09:21 -0500

    query builder is partially functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 const apiKey = "D961BC3F959A416CB66BA38ED853CB39";
-const baseURL = `https://api.rainforestapi.com/request?api_key=${apiKey}`;
+const baseURL = `https://api.rainforestapi.com/request?api_key=${apiKey}&type=search&amazon_domain=amazon.com`;
 
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll("select");
   var instances = M.FormSelect.init(elems, {});
 });
 
+function isPrimeDelivery() {
+  return document.getElementById("prime-delivery").checked;
+}
+
+// returns the selected category's specific amazon category id
+// appended to our query by: category_id=getCategories()
+// If no catgeory was selected at submit time an empty string will be returned
+function getCategory() {
+  var e = document.getElementById("product-categories");
+  return e.options[e.selectedIndex].value;
+}
 
 // Each chip tag will be added to our query in the `search_term` parameter
 // For example: 2chips with "memory cards" "64GB" will be appended to
@@ -36,22 +47,26 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// returns the selected category's specific amazon category id
-// appended to our query by: category_id=getCategories()
-// If no catgeory was selected at submit time an empty string will be returned
-function getCategories() {
-  var e = document.getElementById("product-categories");
-  return e.options[e.selectedIndex].value;
+function createQuery() {
+  let queryURL = baseURL;
+
+  if (keywords.size) {
+    queryURL += `&search_term=${Array.from(keywords).join('+')}`
+  }
+
+  var categoryId = getCategory();
+  if (categoryId) {
+    queryURL += `&category_id=${categoryId}`;
+  }
+  console.log("Query URL constructed: ", queryURL);
+  return queryURL;
 }
-
-
-
 
 // The submit button
 document.getElementById('search-button').addEventListener('click', function(event) {
   event.preventDefault();
   console.log("keyword chips are: ", keywords);
+  createQuery();
+  console.log('prime delivery:>',   isPrimeDelivery());
 
-
-  getCategories();
 });
