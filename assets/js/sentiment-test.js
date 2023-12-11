9e143edb7aac5ac982ab0cc533beb7b0c31c2cd4
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-10 17:17:59 -0700

    created sentimentAnalysis function witht the fetch request

--- assets/js/sentiment-test.js
+++ assets/js/sentiment-test.js
@@ -1,31 +1,40 @@
-// testReviews is an array of object. We want to itirate and use testReviews[i].body as text
+// other scriptfiles only have to call one function to iterate over the top 5 reviews for the product
+// sentimentAnalysis(reviewsArray), reviewsArray is an array of objects containing review info
 
-sentimentArray = [];
+//------------------------------------------------------------------------------//
+//-------this block has to be deleted before submitting project-----------------//
+//------------------------------------------------------------------------------//
 
-for (var i = 0; i < 5; i++) {
-  const url = "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/";
-  const options = {
-    method: "POST",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      "X-RapidAPI-Key": "5901c52a65msh8bbb26b5dff2e05p11e8f9jsn5ad0d7fa75e8",
-      "X-RapidAPI-Host": "twinword-sentiment-analysis.p.rapidapi.com",
-      "Retry-After": 3, //in case it fails we want to wait three seconds to try again
-    },
-    body: new URLSearchParams({
-      text: testReviews[i].body,
-    }),
-  };
-  // console.log(testReviews[i].body);
-  // console.log(testReviews[i].body.length);
-  postData(url, options);
-  // console.log(sentiment);
-  //   sentimentArray.push(sentiment);
+sentimentAnalysis(testReviews);
+
+//------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------//
+
+var sentimentArray = [];
+
+function sentimentAnalysis(reviewsArray) {
+  for (var i = 0; i < 5; i++) {
+    const url = "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/";
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "5901c52a65msh8bbb26b5dff2e05p11e8f9jsn5ad0d7fa75e8",
+        "X-RapidAPI-Host": "twinword-sentiment-analysis.p.rapidapi.com",
+        "Retry-After": 60, //in case it fails we want to wait three seconds to try again
+      },
+      body: new URLSearchParams({
+        text: reviewsArray[i].body,
+      }),
+    };
+
+    postData(url, options);
+  }
 }
 
-// console.log(sentimentArray);
-var sentimentObject = {};
 function postData(url, options) {
+  //this function is to create an API request to twinword with the content of the review
   fetch(url, options)
     .then(function (response) {
       return response.json();
@@ -38,6 +47,8 @@ function postData(url, options) {
 }
 
 function getSentimentArray(sentiment) {
+  //this function will save the current sentiment response in an array
   sentimentArray.push(sentiment);
   console.log(sentimentArray);
+  return sentimentArray;
 }
